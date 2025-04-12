13c17775e4ed0c9695b7700dc15ac6faa81c5bb9
Kori Miyohashi <Miyohashikori457@Gmail.com>
2025-04-13 00:40:39 +0800

    fix

--- setup.py
+++ setup.py
@@ -7,14 +7,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="param-controller",
-    version="0.1.0",
+    version="0.1.1",
     author="Kori-Sama",
     author_email="Miyohashikori457@gmail.com",
     description="A lightweight parameter remote controller for embedded device debugging",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Kori-Sama/param-ctl",
-    packages=find_packages(exclude=['param_ctl.templates.*', 'param_ctl.static.*']),
+    packages=find_packages(
+        exclude=['param_ctl.templates.*', 'param_ctl.static.*']),
     include_package_data=True,
     package_data={
         'param_ctl': [
